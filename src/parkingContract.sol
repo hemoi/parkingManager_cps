577a5c06658c4eebbc5a3c5c5b7a11031fd7977d
hemoi <happy97cjh@gmail.com>
2020-05-10 18:00:20 +0900

    add smartcontract

---
+++ src/parkingContract.sol
@@ -0,0 +1,49 @@
+pragma solidity 0.6.4;
+
+contract parkingManager{
+    
+    // fee
+    uint etherPerTime = 0.01 ether;
+
+    // manager's address
+    address payable owner;
+
+    event SuccessPayment(uint fee);
+    event QueryBalance(uint balance);
+
+    function payMoney(uint8 time) external payable{
+        uint fee = etherPerTime * time;
+        require(msg.value >= fee, "not enough money");
+        address(this).transfer(fee);
+
+        // emit Event
+        emit SuccessPayment(fee);
+
+        msg.sender.transfer(msg.value - fee);
+    }
+
+    function withdraw() external payable onlyOwner returns (string memory){
+        require(address(this).balance > 0, "withdraw Failed");
+
+        owner.transfer(address(this).balance);
+        return("withdraw Success");
+    }
+
+    function queryEther() external view returns (uint){
+        return(address(this).balance);
+    }
+
+    modifier onlyOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    // call when contract create
+    constructor () public {
+        owner = msg.sender;
+    }
+    
+    fallback () external payable {
+
+    }
+}
