f73dc1f8ad003dcd433b2a32774c8518b73e0022
dooyeong20 <59571954+dooyeong20@users.noreply.github.com>
2020-05-11 11:59:54 +0900

    Update parking.js

--- web/parking.js
+++ web/parking.js
@@ -8,7 +8,7 @@ function makeParkingPlace(number) {
     for (let i = 1; i <= number; i++) {
         const parkingPlace = document.createElement("div");
         parkingPlace.classList.add("parking_place");
-        parkingPlace.innerText = "HOTEL \n NUMBER";
+        parkingPlace.innerText = "CAR \n NUMBER";
         parkingPlace.id = i;
         if (i <= number / 2) {
             parkingPlace.classList.add("upside");
