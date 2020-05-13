3779ede05437dfb99b800bc83c215a3c4ffe32ac
hemoi <happy97cjh@gmail.com>
2020-05-13 18:32:54 +0900

    update flask

--- src/App.py
+++ src/App.py
@@ -1,27 +1,49 @@
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, abort, Api, Resource, fields
+from plate_recognizer import plateProcessor, plateRecongizer
 
 app = Flask(__name__)
 api = Api(app)
 
-users = {}
+Users = {}
 
 def abortIfNothing(userID):
-    if userID not in users:
+    if userID not in Users:
         about(404, message="UserID {} doesn't exist".format(userID))
 
 parser = reqparse.RequestParser()
-parser.add_argument('users')
+parser.add_argument('Users', required=True)
 
-class Users(Resource):
-    def get(self, userId):
+
+class UserClass(Resource):
+    def get(self, userID):
         abortIfNothing(userID)
-        return users[userId]
+        return Users[userID]
 
     def delete(self, userID):
         abortIfNothing(userID)
-        del users[userID]
+        del Users[userID]
         return '', 204
 
-    def put(self, userID):
-        args = 
+class UserList(Resource):
+    def get(self):
+        return Users
+
+
+api.add_resource(UserClass, '/users/<userID>')
+api.add_resource(UserList, '/users')
+
+def generator(imgsrc, location):
+    user = {}
+    img = plateProcessor(imgsrc)
+    userID = plateRecongizer(img)
+
+    user['location'] = location
+
+    Users[userID] = user
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
