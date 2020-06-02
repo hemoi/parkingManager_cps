fb1c040c72f0d9ec4f9bdcfc6f126a409bcb54c0
hemoi <happy97cjh@gmail.com>
2020-06-02 22:26:13 +0900

    fix all bugs & change dir, src file name

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource, fields
 from plate_recognizer import plateProcessor, plateRecongizer
-from web3py import Wallet
+from wallet import Wallet
 from web3 import Web3
 from flask_cors import CORS, cross_origin
 from datetime import datetime
@@ -30,12 +30,11 @@ def abortIfNothing(userID):
 
 # calculate FEE
 def timeCalculator(enterTime):
-    deltaTime = enterTime - datetime.now()
-    seconds = deltaTime.seconds
-    seconds += deltaTime.minute * 60
-    seconds += deltaTime.hour * 60 * 60
-
-    return seconds
+    checkTime = datetime.now() - enterTime
+    print(str(checkTime.seconds) + " seconds")
+    print("IN  : " + enterTime.isoformat())
+    print("OUT : " + datetime.now().isoformat())
+    return checkTime.seconds
 
 
 parser = reqparse.RequestParser()
@@ -49,10 +48,10 @@ class User():
 
 # Users who park their cars
 class ParkingUser():
-    def __init__(self, User, location):
+    def __init__(self, User, location, enterTime):
         self.user = User
         self.userLocation = location
-        self.enterTime = datetime.now()
+        self.enterTime = enterTime
     
     def getInfo(self):
         tmp = {}
@@ -73,22 +72,10 @@ walletList.append(wallet1)
 walletList.append(wallet2)
 walletList.append(wallet3)
 
-# user1.setID('user1')
-# user2.setID('user2')
-# user3.setID('user3')
-
-# Users[user1.id] = user1
-# Users[user2.id] = user2
-# Users[user3.id] = user3
-
-# parking1 = ParkingUser(user1,'1A')
-# parkingUsers[parking1.user.id] = parking1.getInfo()
-
-# parking2 = ParkingUser(user2,'1B')
-# parkingUsers[parking2.user.id] = parking2.getInfo()
 
 ### FLASK
 class UserClass(Resource):
+    
     ## GET
     def get(self, userID):
         abortIfNothing(userID)
@@ -102,19 +89,27 @@ class UserClass(Resource):
         
         ## make transaction to owner
         ## parking fee is 0.01 ether * seconds (for testing)
-        enterTime = parkingUsers[userID]['enterTime']
+        enterTime = parkingUsers[userID].enterTime
         tToSeconds = timeCalculator(enterTime)
-        tx = Users[userID].wallet.makeTransaction(tToSeconds)
-        User.wallet.sendTransaction(tx)
+        tx = Users[userID].wallet.makeTransaction(tToSeconds, enterTime.isoformat())
+        Users[userID].wallet.sendTransaction(tx)
 
-        # del parkingUsers[userID]
+        parkingAvailable.append(parkingUsers[userID].userLocation)
+        del parkingUsers[userID]
+
         return '', 204
 
 
 ### return all parkingUsers List
 class UserList(Resource):
     def get(self):
-        return parkingUsers
+        allData = []
+        print(len(parkingUsers))
+        for iParkingUser in parkingUsers.values():
+            data = iParkingUser.getInfo()
+            allData.append(data)
+        print(allData)
+        return allData
 
 api.add_resource(UserClass, '/users/<userID>')
 api.add_resource(UserList, '/users')
@@ -153,9 +148,9 @@ class MyHandler(FileSystemEventHandler):
         # find available location
         location = parkingAvailable.pop()
 
-        # add to ParkingUsers DIC
-        tmpParkingUser = ParkingUser(userInfo, location)
-        parkingUsers[tmpParkingUser.user.id] = tmpParkingUser.getInfo()
+        # add to ParkingUsers DICT
+        tmpParkingUser = ParkingUser(userInfo, location, datetime.now())
+        parkingUsers[plateNum] = tmpParkingUser
 
         print(plateNum + " enter")
 
--- src/test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(os.getcwd())
-watchDir = os.getcwd()
-print(os.path.join(watchDir, 'src', 'img'))
